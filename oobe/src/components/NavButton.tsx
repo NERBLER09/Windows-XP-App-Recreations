efa10e2acce569a2cf806c15cc319fa10f7a7ed8
Noah Beaudin <noah.beaudin1@gmail.com>
2021-06-03 19:52:05 -0600

    OOBE - Added hover animations for the nav buttons

--- oobe/src/components/NavButton.tsx
+++ oobe/src/components/NavButton.tsx
@@ -1,10 +1,16 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "../styles/NavButton.css"
 import "../styles/Footer.css"
 
 import back from "../assets/backup.jpg"
+import backOver from "../assets/backover.jpg"
+import backDown from "../assets/backdown.jpg"
 import next from "../assets/nextup.jpg"
+import nextOver from "../assets/nextover.jpg"
+import nextDown from "../assets/nextdown.jpg"
 import skip from "../assets/skipup.jpg"
+import skipOver from "../assets/skipover.jpg"
+import skipDown from "../assets/skipdown.jpg"
 
 export interface NavButtonsProps {
     type: string,
@@ -15,6 +21,15 @@ export interface NavButtonsProps {
 }
  
 const NavButtons: FC<NavButtonsProps> = ({text, type, buttonType, location, backLocation}) => {
+    const [nextButtonHovered, setNextButtonHovered]: any = useState(false)
+    const [nextButtonClicked, setNextButtonClicked] = useState(false)
+
+    const [backButtonHovered, setBackButtonHovered]: any = useState(false)
+    const [backButtonClicked, setBackButtonClicked] = useState(false)
+
+    const [skipButtonHovered, setSkipButtonHovered]: any = useState(false)
+    const [skipButtonClicked, setSkipButtonClicked] = useState(false)
+
     const navToNextScreen = (location: string) => {
         // Closes the tab once done the oobe
         if(window.location.pathname === "/finish") {
@@ -36,18 +51,50 @@ const NavButtons: FC<NavButtonsProps> = ({text, type, buttonType, location, back
                 <div className={type} >
                     <div id="nav-item" onClick={() => navToNextScreen(location)}>
                         <p className="nav-text">{text}</p>
-                        <div id="next-button" className="nav-img">
-                            <img src={next} alt="" />
-                        </div>
+                        {nextButtonHovered === false &&
+                            <div id="next-button" className="nav-img" onMouseEnter={() => setNextButtonHovered(!nextButtonHovered)}>
+                                <img src={next} alt="" />
+                            </div>
+                        }                       
+                        {nextButtonHovered === true &&
+                            <div id="next-button" className="nav-img" onMouseLeave={() => setNextButtonHovered(!nextButtonHovered)}
+                            onClick={() => {
+                                setNextButtonClicked(!nextButtonClicked)
+                                setNextButtonHovered("")
+                            }}>
+                                <img src={nextOver} alt="" />
+                            </div>
+                        }
+                        {nextButtonClicked === true && 
+                            <div id="next-button" className="nav-img">
+                                <img src={nextDown} alt="" />
+                            </div>
+                        }
                     </div>
                 </div>
             }
             {buttonType === "back" && 
                 <div className={type} >
                      <div id="nav-item" onClick={() => goBack()}>
-                        <div id="back-button" className="nav-img">
-                            <img src={back} alt="" />
-                        </div>
+                        {backButtonHovered === false &&
+                            <div id="next-button" className="nav-img" onMouseEnter={() => setBackButtonHovered(!backButtonHovered)}>
+                                <img src={back} alt="" />
+                            </div>
+                        }                       
+                        {backButtonHovered === true &&
+                            <div id="next-button" className="nav-img" onMouseLeave={() => setSkipButtonHovered(!backButtonHovered)}
+                            onClick={() => {
+                                setBackButtonClicked(!backButtonClicked)
+                                setBackButtonHovered("")
+                            }}>
+                                <img src={backOver} alt="" />
+                            </div>
+                        }
+                        {backButtonClicked === true && 
+                            <div id="next-button" className="nav-img">
+                                <img src={backDown} alt="" />
+                            </div>
+                        }
                         <p className="nav-text">{text}</p>
                     </div>
                 </div>
@@ -56,9 +103,25 @@ const NavButtons: FC<NavButtonsProps> = ({text, type, buttonType, location, back
                 <div className={type} >
                     <div id="nav-item" onClick={() => navToNextScreen(location)}>
                         <p className="nav-text">{text}</p>
-                        <div id="skip-button" className="nav-img">
-                            <img src={skip} alt="" />
-                        </div>
+                        {skipButtonHovered === false &&
+                            <div id="next-button" className="nav-img" onMouseEnter={() => setSkipButtonHovered(!skipButtonHovered)}>
+                                <img src={skip} alt="" />
+                            </div>
+                        }                       
+                        {skipButtonHovered === true &&
+                            <div id="next-button" className="nav-img" onMouseLeave={() => setSkipButtonHovered(!skipButtonHovered)}
+                            onClick={() => {
+                                setSkipButtonClicked(!skipButtonClicked)
+                                setSkipButtonHovered("")
+                            }}>
+                                <img src={skipOver} alt="" />
+                            </div>
+                        }
+                        {skipButtonClicked === true && 
+                            <div id="next-button" className="nav-img">
+                                <img src={skipDown} alt="" />
+                            </div>
+                        }
                     </div>
                 </div>
             }
