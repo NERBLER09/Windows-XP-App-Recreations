015b4389090819fd95befa35e8b00b02fc8b7d60
Noah Beaudin <noah.beaudin1@gmail.com>
2021-06-09 16:50:04 -0600

    MS-Paint - Updated how windows controls work

--- ms-paint/src/windowControls.js
+++ ms-paint/src/windowControls.js
@@ -1,6 +1,5 @@
-const electron = window.require('electron')
+const electron = require('electron')
 const {ipcRenderer} = electron;
-
 // Closes the window 
 const closeWindow = () => {
     window.close()
@@ -11,7 +10,16 @@ const maximizeWindow = () => {
     ipcRenderer.send("maximize")
 }
   
-// Minimizes the window
+// Minimizes the window 
 const minimizeWindow = () => {
     ipcRenderer.send("minimize")
 }
+
+const closeButton = document.getElementById("closeButton")
+closeButton.addEventListener("click", closeWindow)
+
+const maximizeButton = document.getElementById("maximizeButton")
+maximizeButton.addEventListener("click", maximizeWindow)
+
+const minimizeButton = document.getElementById("minimizeButton")
+minimizeButton.addEventListener("click", minimizeWindow)
